e67a89d37706ba8abd07a5e37e895ef0b43fed77
Evgenia Afanasieva <electrojanet@gmail.com>
2023-01-15 23:38:55 +0300

    Added folder Example001

---
+++ Example001/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.WriteLine("Введите два числа:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+int min = number2;
+if (number2 > max)
+{
+    max = number2;
+    min = number1;
+}
+System.Console.Write("max = ");
+System.Console.WriteLine(max);
+System.Console.Write("min = ");
+System.Console.WriteLine(min);
