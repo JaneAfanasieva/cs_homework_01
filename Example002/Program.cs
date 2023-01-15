7fa4cb4978989370921266cc67c4223f135e4a4b
Evgenia Afanasieva <electrojanet@gmail.com>
2023-01-15 23:50:41 +0300

    Added folder Examole002

---
+++ Example002/Program.cs
@@ -0,0 +1,15 @@
+﻿System.Console.WriteLine("Введите три числа:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+if (number2 > max)
+{
+    max = number2;
+}
+if (number3 > max)
+{
+    max = number3;
+}
+System.Console.Write("max = ");
+System.Console.WriteLine(max);
