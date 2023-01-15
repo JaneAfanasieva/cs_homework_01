b44c9e349ef35a5f42df1037ed2798c0397136c2
Evgenia Afanasieva <electrojanet@gmail.com>
2023-01-15 23:53:16 +0300

    Added folder Example003

---
+++ Example003/Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.WriteLine("Введите любое число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+    System.Console.WriteLine("Четное число");
+}
+else
+{
+    System.Console.WriteLine("Нечетное число");
+}
