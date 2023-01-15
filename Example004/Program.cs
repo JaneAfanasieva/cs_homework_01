e19bb6519715a3c3afa85bcf33307f5c02761b09
Evgenia Afanasieva <electrojanet@gmail.com>
2023-01-15 23:55:57 +0300

    Added folder Example004

---
+++ Example004/Program.cs
@@ -0,0 +1,13 @@
+﻿System.Console.WriteLine("Введите любое число начиная от 1:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 1) 
+{
+    System.Console.WriteLine("Вы ввели число меньше 1");
+    return;
+}
+int curentNumber = 2;
+while (curentNumber <= number)
+{
+    System.Console.WriteLine(curentNumber);
+    curentNumber = curentNumber + 2;
+}   
